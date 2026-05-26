ad7dfc43847a34e89405cc92b25103a72b3c73aa
aycom366 <bamigboyeayomide200@gmail.com>
2026-05-26 11:48:35 +0100

    chore: add dot env java for the app

--- creatorstore/src/main/java/com/aycom/creatorstore/CreatorstoreApplication.java
+++ creatorstore/src/main/java/com/aycom/creatorstore/CreatorstoreApplication.java
@@ -3,10 +3,17 @@ package com.aycom.creatorstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class CreatorstoreApplication {
 
 	public static void main(String[] args) {
+
+		// dot env configuration
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(CreatorstoreApplication.class, args);
 	}
 
