6e2c94ee22dcd601fa4f6663c19201b05b29d90d
aycom366 <bamigboyeayomide200@gmail.com>
2026-05-26 10:26:14 +0100

    chore: basic setup

---
+++ creatorstore/src/main/java/com/aycom/creatorstore/CreatorstoreApplication.java
@@ -0,0 +1,13 @@
+package com.aycom.creatorstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatorstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatorstoreApplication.class, args);
+	}
+
+}
---
+++ creatorstore/src/test/java/com/aycom/creatorstore/CreatorstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aycom.creatorstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
