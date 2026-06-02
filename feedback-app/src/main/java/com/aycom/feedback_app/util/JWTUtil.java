8dd631cadc5dd3cc44c17d965da70dc1f3bfc383
aycom366 <bamigboyeayomide200@gmail.com>
2026-06-03 00:38:08 +0100

    fix: moved jwt secret to dotenv

--- feedback-app/src/main/java/com/aycom/feedback_app/util/JWTUtil.java
+++ feedback-app/src/main/java/com/aycom/feedback_app/util/JWTUtil.java
@@ -14,7 +14,7 @@ public class JWTUtil {
 
     private static final long EXPIRATION_MS = 1000 * 60 * 60;
     private static final SecretKey SECRET_KEY = Keys.hmacShaKeyFor(
-            "your-256-bit-secret-key-change-this-in-prod".getBytes());
+            System.getenv("JWT_SECRET").getBytes());
 
     public String generateToken(String email) {
         return Jwts.builder()
