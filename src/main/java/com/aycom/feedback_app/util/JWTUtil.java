6af53cd6f5245c1a3248f591700da07e51529336
aycom366 <bamigboyeayomide200@gmail.com>
2026-06-03 20:19:36 +0100

    refactor: move JWT secret to application.yml and initialize SECRET_KEY in JWTUtil

--- src/main/java/com/aycom/feedback_app/util/JWTUtil.java
+++ src/main/java/com/aycom/feedback_app/util/JWTUtil.java
@@ -1,20 +1,34 @@
 package com.aycom.feedback_app.util;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 import javax.crypto.SecretKey;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 
 @Component
 public class JWTUtil {
 
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
     private static final long EXPIRATION_MS = 1000 * 60 * 60;
-    private static final SecretKey SECRET_KEY = Keys.hmacShaKeyFor(
-            System.getProperty("JWT_SECRET").getBytes());
+    private SecretKey SECRET_KEY;
+
+    /**
+     * PostConstruct marks a method to run once, automatically, after spring has
+     * finished injecting all dependencies like jwtSecret
+     */
+    @PostConstruct
+    public void init() {
+        this.SECRET_KEY = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+    }
 
     public String generateToken(String email) {
         return Jwts.builder()
